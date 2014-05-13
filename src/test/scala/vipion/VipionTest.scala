01b8bd086ada24e42b721ed8c9ef6caa4305f2f7
Sébastien Doeraene <sjrdoeraene@gmail.com>
2014-05-13 12:28:48 +0200

    Add rudimentary test suite.

--- src/test/scala/vipion/VipionTest.scala
+++ src/test/scala/vipion/VipionTest.scala
@@ -6,14 +6,36 @@ import scala.scalajs.test.JasmineTest
 
 object VipionTest extends JasmineTest {
 
-  describe("Vipion") {
+  import Player._
+  import SquareState._
 
-    it("should implement square()") {
-      import Vipion._
+  describe("VipionGame") {
 
-      expect(square(0)).toBe(0)
-      expect(square(4)).toBe(16)
-      expect(square(-5)).toBe(25)
+    it("should create a diagonal board") {
+      val game = VipionGame.diagonal
+      import game._
+      expectTrue(board(0, 0) == Empty)
+      expectTrue(board(0, 1) == Empty)
+      expectTrue(board(1, 1) == Disabled)
+      expectTrue(board(1, 2) == Empty)
+      expectTrue(board(2, 2) == Disabled)
+      expectFalse(done)
+      expectFalse(winner.isDefined)
+    }
+
+    it("should declare a winner") {
+      val game = VipionGame.diagonal
+        .playAt(0, 0) .playAt(3, 0)
+        .playAt(0, 1) .playAt(3, 1)
+        .playAt(0, 2)
+      expectTrue(game.done)
+      expectTrue(game.winner == Some(Cross))
     }
   }
+
+  private def expectTrue(test: Boolean): Unit =
+    expect(test).toBeTruthy
+
+  private def expectFalse(test: Boolean): Unit =
+    expect(test).toBeFalsy
 }

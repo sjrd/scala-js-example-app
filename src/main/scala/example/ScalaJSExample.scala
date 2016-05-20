0fcde3c31db482663758fafb24faf24514265702
Sébastien Doeraene <sjrdoeraene@gmail.com>
2016-05-20 20:45:22 +0200

    wip

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -4,11 +4,31 @@ import scala.scalajs.js
 import js.annotation.JSExport
 import org.scalajs.dom
 
+@js.native
+trait EventName extends js.Object {
+  type EventType <: dom.Event
+}
+
+object EventName {
+  def apply[T <: dom.Event](name: String): EventName { type EventType = T } =
+    name.asInstanceOf[EventName { type EventType = T }]
+
+  val onmousedown = apply[dom.MouseEvent]("onmousedown")
+}
+
+@js.native
+trait ElementExt extends js.Object {
+  def addEventListener(name: EventName)(
+      f: js.Function1[name.EventType, _]): Unit
+}
+
 object ScalaJSExample extends js.JSApp {
   def main(): Unit = {
     val paragraph = dom.document.createElement("p")
     paragraph.innerHTML = "<strong>It works!</strong>"
     dom.document.getElementById("playground").appendChild(paragraph)
+
+    val p = paragraph.asInstanceOf[ElementExt]
   }
 
   /** Computes the square of an integer.
